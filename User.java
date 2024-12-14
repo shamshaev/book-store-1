ac77619b21697340b3123c8ce9dc8a4156a3fa41
shamshaev <an.shamshaev@gmail.com>
2024-12-15 01:36:18 +0300

    Add toString

--- User.java
+++ User.java
@@ -22,4 +22,12 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
